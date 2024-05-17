d0752c8ec7bd64fc1aacd4690d11b4aa79d2f8a8
Ayo Ayco <ayo@ayco.io>
2024-05-17 18:04:58 +0200

    feat: use config.json for sentry stuff

--- web.py
+++ web.py
@@ -1,3 +1,5 @@
+import sentry_sdk
+import json
 from flask import Flask, send_from_directory
 from partials import partials
 from threads import threads
@@ -5,6 +7,17 @@ from threads import threads
 app = Flask(__name__)
 app.register_blueprint(partials, url_prefix='/p')
 app.register_blueprint(threads, url_prefix='/threads')
+app.config.from_file("config.json", load=json.load)
+
+# sentry.io error monitoring
+sentry_config = app.config["SENTRY"]
+sentry_sdk.init(
+    dsn=sentry_config["dsn"],
+    traces_sample_rate=sentry_config["traces_sample_rate"],
+    profiles_sample_rate=sentry_config["profiles_sample_rate"],
+    enable_tracing=sentry_config["enable_tracing"],
+)
+
 
 @app.route('/')
 def home():
@@ -17,9 +30,16 @@ def dist(path):
     else:
         return send_from_directory('dist', path + '/index.html')
 
+@app.route('/error')
+def errorthing():
+	1/0
+	return 'hello'
+
 @app.errorhandler(404)
 def not_found(e):
     return send_from_directory('dist', '404.html'), 404
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
