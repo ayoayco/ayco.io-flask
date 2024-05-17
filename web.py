792653568563b292202e69be9de1d7303214ceec
Ayo Ayco <ayo@ayco.io>
2024-05-17 18:12:44 +0200

    feat: remove /error route

--- web.py
+++ web.py
@@ -30,16 +30,10 @@ def dist(path):
     else:
         return send_from_directory('dist', path + '/index.html')
 
-@app.route('/error')
-def errorthing():
-	1/0
-	return 'hello'
-
 @app.errorhandler(404)
 def not_found(e):
     return send_from_directory('dist', '404.html'), 404
 
-
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
