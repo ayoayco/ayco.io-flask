bc46633af45d68c2200d68915c3227a3248003d2
Ayo Ayco <ayo@ayco.io>
2024-05-18 21:53:36 +0200

    refactor: put threads blueprint in threads dir

--- web.py
+++ web.py
@@ -3,7 +3,7 @@ from flask_caching import Cache
 import sentry_sdk
 import json
 from partials import partials
-from threads import threads
+from threads.threads import threads
 
 app = Flask(__name__)
 app.register_blueprint(partials, url_prefix='/p')
@@ -12,16 +12,18 @@ app.config.from_file("config.json", load=json.load)
 
 # caching
 cache = Cache()
+cache.init_app(app)
+print(' * Cache type: ' + app.config["CACHE_TYPE"])
 
 # perf monitoring & error tracking
 sentry_config = app.config["SENTRY"]
+print(' * Monitoring DSN: ' + sentry_config["dsn"])
 sentry_sdk.init(
     dsn=sentry_config["dsn"],
     traces_sample_rate=sentry_config["traces_sample_rate"],
     profiles_sample_rate=sentry_config["profiles_sample_rate"],
     enable_tracing=sentry_config["enable_tracing"],
 )
-
 
 @app.route('/')
 def home():
